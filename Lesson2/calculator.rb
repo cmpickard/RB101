8a4f234ae9c071bd58df8f35a600b1d60422e923
Caleb P <caleb.pickard@gmail.com>
2023-02-08 23:47:45 +0000

    Calculator now uses floats rather than ints

--- Lesson2/calculator.rb
+++ Lesson2/calculator.rb
@@ -10,14 +10,14 @@ so you have to call chomp() to remove it: Kernel.gets().chomp().
 =end
 
 #single method for all 4 operations
-def math(int1,int2, operation)
+def math(float1,float2, operation)
   case operation
-  when "add" then return int1+int2
-  when "subtract" then return int1 - int2
-  when "multiply" then return int1 * int2
+  when "add" then return float1+float2
+  when "subtract" then return float1 - float2
+  when "multiply" then return float1 * float2
   else 
-    if int2 != 0
-      return int1/int2
+    if float2 != 0
+      return float1/float2
     else return "that you can't divide by zero"
     end
   end
@@ -25,22 +25,22 @@ end
 
 #Ask for two numbers and an operation
 puts "Please provide your first number.(Non-number input will be treated as 0)"
-int1 = gets.chomp.to_i
+float1 = gets.chomp.to_f
 puts "Please provide your second number.(Non-number input will be treated as 0)"
-int2 = gets.chomp.to_i
+float2 = gets.chomp.to_f
 
 operation = nil
 
 while operation == nil
-  puts "What would like to do with these? (add, subtract, multiply, divide)"
+  puts "What would like to do w/ your nums? (add, subtract, multiply, divide)"
   input3 = gets.chomp
   if input3 == "add" || input3 == "subtract" || input3 == "multiply" || input3 == "divide"
     operation = input3
   else 
-    puts "That's not a valid operation"
+    puts "That's not a valid operation!"
   end
 end
 
 #Call the math method and then print result
-answer = math(int1,int2,operation)
-puts "When we #{operation} #{int1} and #{int2} the result is: #{answer}!"
+answer = math(float1,float2,operation)
+puts "When we #{operation} #{float1} and #{float2} the result is: #{answer}!"
